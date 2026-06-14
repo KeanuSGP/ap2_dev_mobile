7c19794d1031854071663e5df1f4c4e80e0d277e
Keanu <keanusantosgermanopereira@gmail.com>
2026-06-14 17:51:01 -0300

    adicao de propriedade tipo usuario no dto de login

--- backend/src/main/java/com/keanusantos/ap2_dev_mobile/dto/LoginResponseDTO.java
+++ backend/src/main/java/com/keanusantos/ap2_dev_mobile/dto/LoginResponseDTO.java
@@ -1,6 +1,8 @@
 package com.keanusantos.ap2_dev_mobile.dto;
 
+import com.keanusantos.ap2_dev_mobile.entity.Usuario;
+
 public record LoginResponseDTO(
-        String token
+        String token, Usuario usuario
 ) {
 }
