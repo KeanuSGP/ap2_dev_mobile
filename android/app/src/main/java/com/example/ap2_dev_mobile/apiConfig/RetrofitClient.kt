9cb8eee8baafe8b0adde11f7783acb9d0ba6518a
KEANU SANTOS GERMANO PEREIRA <202503341664@alunos.ibmec.edu.br>
2026-06-15 09:42:52 -0300

    correção url api

--- android/app/src/main/java/com/example/ap2_dev_mobile/apiConfig/RetrofitClient.kt
+++ android/app/src/main/java/com/example/ap2_dev_mobile/apiConfig/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.3:8080/"
+    private const val BASE_URL = "http://192.168.56.1:8080/"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
